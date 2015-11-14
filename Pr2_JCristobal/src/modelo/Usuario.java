e5df6b4256265184f6575dcc28bfc2b0a622df38
JCristobal <tobas92@gmail.com>
2015-11-14 10:29:10 +0100

    Añado documentación a clase Usuario

--- Pr2_JCristobal/src/modelo/Usuario.java
+++ Pr2_JCristobal/src/modelo/Usuario.java
@@ -1,5 +1,9 @@
 package modelo;
-
+/**
+ * 
+ * @author jcristobal
+ *
+ */
 
 public class Usuario {
 
@@ -9,18 +13,32 @@ public class Usuario {
 	private String nombre;
 	private String apellido;	
 	
+	/**
+	 * Constructor de un usuario vacío
+	 */
 	public Usuario (){
 		email = "";
 		nombre = "";
 		apellido = "";
 	}
-	
+	/**
+	 * Constructor de usuario según 3 parámetros
+	 * @param email correo electrónico del usuario
+	 * @param nombre nombre del usuario
+	 * @param apellido apellidos del usuario
+	 */
 	public Usuario (String email, String nombre, String apellido){
 		this.email = email;
 		this.nombre = nombre;
 		this.apellido = apellido;
 	}
-
+	/**
+	 * Constructor del usuario seǵun 4 parámetros
+	 * @param email correo electrónico del usuario
+	 * @param nombre nombre del usuario
+	 * @param apellido apellidos del usuario
+	 * @param idUsuario identificador del usuario
+	 */
 	public Usuario(String email, String nombre, String apellido, long idUsuario) {
 		this.email = email;
 		this.nombre = nombre;
@@ -29,56 +47,56 @@ public class Usuario {
 	}
 
 	/**
-	 * @return 
+	 * @return identificador del usuario
 	 */
 	public long getIdUsuario() {
 		return idUsuario;
 	}
 
 	/**
-	 * @param 
+	 * @param cambia el identificador del usuario
 	 */
 	public void setIdUsuario(long idUsuario) {
 		this.idUsuario = idUsuario;
 	}
 	
 	/**
-	 * @return 
+	 * @return correo electrónico del usuario
 	 */
 	public String getEmail() {
 		return email;
 	}
 	
 	/**
-	 * @param 
+	 * @param cambia el correo electrónico del usuario
 	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
 	
 	/**
-	 * @return 
+	 * @return nombre del usuario
 	 */
 	public String getNombre() {
 		return nombre;
 	}
 	
 	/**
-	 * @param nombre 
+	 * @param cambia el nombre del usuario
 	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 	
 	/**
-	 * @return 
+	 * @return  apellidos del usuario
 	 */
 	public String getApellido() {
 		return apellido;
 	}
 	
 	/**
-	 * @param 
+	 * @param  cambia apellidos del usuario
 	 */
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
